84af8fe39663c08c34ee3b673395b8ac0fc38f56
aniruddha <aniruddha@aniruddha>
2016-04-27 12:29:18 +0530

    initial commit

---
+++ src/main/java/com/codekul/firstgradle/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codekul.firstgradle;
+
+import org.apache.commons.lang.time.DateUtils;
+
+/**
+ *
+ * @author aniruddha
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        DateUtils dateUtils = new DateUtils();
+    }
+}
